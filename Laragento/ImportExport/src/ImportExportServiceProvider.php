4454ac666d0b11d366b0ee241690ab2d8567aa92
Stefan Mettler <stefan.mettler92@gmail.com>
2018-03-22 17:21:51 +0100

    Added module import-export

---
+++ Laragento/ImportExport/src/ImportExportServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Laragento\ImportExport;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class ImportExportServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
