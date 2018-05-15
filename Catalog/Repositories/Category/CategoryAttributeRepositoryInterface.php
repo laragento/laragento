668717c60585e8a860f496c655fdeb7e087fce37
philippe bumann <philippe.bumann@mexan.ch>
2018-05-15 10:58:52 +0200

    set storeId param as optional

--- Catalog/Repositories/Category/CategoryAttributeRepositoryInterface.php
+++ Catalog/Repositories/Category/CategoryAttributeRepositoryInterface.php
@@ -6,7 +6,7 @@ interface CategoryAttributeRepositoryInterface
 {
     public function save($categoryData, $categoryId);
 
-    public function data($attributeCode, $categoryId, $storeId);
+    public function data($attributeCode, $categoryId, $storeId = 0);
 
     public function saveEntity($attribute, $entity);
 }
--- Catalog/Repositories/Product/ProductAttributeRepositoryInterface.php
+++ Catalog/Repositories/Product/ProductAttributeRepositoryInterface.php
@@ -6,7 +6,7 @@ interface ProductAttributeRepositoryInterface
 {
     public function save($productData, $productId);
 
-    public function data($attributeCode, $productId, $storeId);
+    public function data($attributeCode, $productId, $storeId = 0);
 
     public function saveEntity($attribute, $entity);
 }
