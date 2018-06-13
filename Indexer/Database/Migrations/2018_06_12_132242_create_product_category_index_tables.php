03d65a6ab84a56af932b5abe97f2edf7822c3270
philippe bumann <philippe.bumann@mexan.ch>
2018-06-13 17:19:51 +0200

    added store ref to index tables

--- Indexer/Database/Migrations/2018_06_12_132242_create_product_category_index_tables.php
+++ Indexer/Database/Migrations/2018_06_12_132242_create_product_category_index_tables.php
@@ -17,20 +17,24 @@ class CreateProductCategoryIndexTables extends Migration
             $table->increments('id');
 
             $table->unsignedInteger('product_id');
+            $table->unsignedInteger('store_id');
 
             $table->timestamps();
 
             $table->foreign('product_id')->references('entity_id')->on('catalog_product_entity');
+            $table->foreign('store_id')->references('store_id')->on('store');
         });
 
         Schema::create('lg_catalog_category_index', function (Blueprint $table) {
             $table->increments('id');
 
             $table->unsignedInteger('category_id');
+            $table->unsignedInteger('store_id');
 
             $table->timestamps();
 
             $table->foreign('category_id')->references('entity_id')->on('catalog_category_product');
+            $table->foreign('store_id')->references('store_id')->on('store');
         });
     }
 
