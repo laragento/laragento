9b87aab447ec3e4c00dffdb93ecc6f2939d136d0
philippe bumann <philippe.bumann@mexan.ch>
2018-06-14 14:30:17 +0200

    implemented indexer for categories

--- Indexer/Console/IndexerUpdateCategories.php
+++ Indexer/Console/IndexerUpdateCategories.php
@@ -53,61 +53,8 @@ class IndexerUpdateCategories extends IndexerCommand
         $categoryAttributes = Config::get('indexer.category_attributes');
         $storeIds = Config::get('indexer.stores');
 
+        $this->syncIndexerCols('lg_catalog_category_index', $categoryAttributes);
 
-
-        foreach($categoryAttributes as $categoryAttribute) {
-            if(!Schema::hasColumn('lg_catalog_category_index', $categoryAttribute)) {
-                //create collumn if not found in table
-                Schema::table('lg_catalog_category_index', function($table) use($categoryAttribute) {
-                    $table->string($categoryAttribute, 255)->default('')->nullable();
-                });
-            }
-        }
-
-        //check last execution time
-        $lastExecutionTimestamp = Cache::get('indexer-update-categories-timestamp');
-
-        $lastExecution = null;
-        $query = DB::table('catalog_category_entity');
-
-        if($lastExecutionTimestamp != null) {
-            $lastExecution = new DateTime();
-            $lastExecution->setTimestamp($lastExecutionTimestamp);
-            $query = $query->where('updated_at', '>', $lastExecution->format('Y-m-d H:i:s'));
-        }
-
-        $this->countUpdates = 0;
-
-        $query->orderBy('entity_id')->chunk(100, function ($categories) use($categoryAttributes, $storeIds) {
-            foreach($categories as $category) {
-                $this->countUpdates++;
-
-                //update attributes in index table for stores
-                foreach($storeIds as $storeId) {
-                    $categoryIndex = CategoryIndex::firstOrNew([
-                        'category_id' => $category->entity_id,
-                        'store_id' => $storeId
-                    ]);
-
-                    foreach($categoryAttributes as $categoryAttribute) {
-                        $data = $this->categoryAttributeRepository->data($categoryAttribute, $category->entity_id, $storeId);
-                        //if data not found for specific storeId, search in default store 0
-                        if(!$data) {
-                            $data = $this->categoryAttributeRepository->data($categoryAttribute, $category->entity_id);
-                        }
-
-                        $categoryIndex->{$categoryAttribute} = $data ? $data->value : '';
-                    }
-
-                    $categoryIndex->save();
-                }
-            }
-        });
-
-        //update last execution timestamp
-        $timestamp = time();
-        print 'Categories updated: ' . $this->countUpdates . "\n";
-        print 'Cache timestamp: ' . $timestamp . "\n";
-        Cache::forever('indexer-update-categories-timestamp', $timestamp);
+        $this->updateIndexerTable('catalog_category_entity', 'indexer-update-categories-timestamp', $categoryAttributes, $storeIds, 'category_id', CategoryIndex::class, $this->categoryAttributeRepository);
     }
 }
