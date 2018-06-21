4d8053f670dc8496d480cef82febdef20beba1c1
tanja.braether <tanja.braether@mexan.ch>
2018-06-21 13:12:48 +0000

    Fix rounding in PriceHelper

--- Core/Helpers/PriceHelper.php
+++ Core/Helpers/PriceHelper.php
@@ -5,9 +5,10 @@ function localPrice($amount, $decimals = 2)
     return number_format($amount, $decimals, '.', "'");
 }
 
-function roundPrice($amount, $decimals = 2, $accuracy)
+function roundPrice($amount, $accuracy, $decimals = 2)
 {
     $calc = 100 / $accuracy;
-    $value = round((($amount + 0.000001) * $calc) / $calc, 2);
+    $value =round(($amount + 0.000001) * $calc) / $calc;
+
     return number_format($value, $decimals, '.', "'");
 }
