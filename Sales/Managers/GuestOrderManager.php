02484a85ee468ea369c6bd8dd51a86075891e28b
tanja.braether <tanja.braether@mexan.ch>
2018-08-31 11:48:55 +0200

    Adjust ordermapper with correct default values

--- Sales/Managers/GuestOrderManager.php
+++ Sales/Managers/GuestOrderManager.php
@@ -14,6 +14,7 @@ class GuestOrderManager extends AbstractOrderManager
     protected function mapQuoteToOrder($quote)
     {
         $store = Store::whereStoreId($quote->getStoreId())->first();
+        $fullStoreName = $store->website->name . "\n\r" . $store->group->name . "\n\r" . $store->name;
         return [
             "state" => self::ORDER_STATE_NEW,
             "status" => self::ORDER_STATUS_PENDING,
@@ -29,22 +30,22 @@ class GuestOrderManager extends AbstractOrderManager
             "base_shipping_tax_amount" => "0.0000", // ToDo Tax Calculation
             "base_subtotal" => $quote->base_subtotal,
             "base_tax_amount" => "0.0000", // ToDo Tax Calculation
-            "base_to_global_rate" => "0.0000", // ToDo Must become Dynamic
-            "base_to_order_rate" => "0.0000", // ToDo Must become Dynamic
-            "base_total_qty_ordered" => $quote->items_qty,
-            "discount_amount" => $quote->subtotal - $quote->subtotal_with_discount,
-            "grand_total" => $quote->grand_total,
+            "base_to_global_rate" => "1.0000", // ToDo Must become Dynamic
+            "base_to_order_rate" => "1.0000", // ToDo Must become Dynamic
+            "base_total_qty_ordered" => null, // ToDo Must become Dynamic
+            "discount_amount" => $quote->subtotal - $quote->base_subtotal_with_discount,
+            "grand_total" => $quote->base_grand_total,
             "shipping_amount" => "0.0000", // ToDo Get from shipping entity
             "shipping_tax_amount" => "0.0000", // ToDo Tax Calculation
             "store_to_base_rate" => "0.0000", // ToDo Must become Dynamic
             "store_to_order_rate" => "0.0000", // ToDo Must become Dynamic
-            "subtotal" => $quote->subtotal,
+            "subtotal" => $quote->base_subtotal,
             "tax_amount" => "0.0000", // ToDo Tax Calculation
             "total_qty_ordered" => $quote->items_qty, // todo check
             "can_ship_partially" => 0,
             "can_ship_partially_item" => 0,
             "customer_is_guest" => 1, //ToDo Is set via Checkout Method "als gast"
-            "customer_note_notify" => "", // ToDo make dynamic
+            "customer_note_notify" => 0, // ToDo make dynamic No idea whats this
             "billing_address_id" => null, // ToDo!! Resave Order
             "customer_group_id" => 0, // Dynamic, if not Guest
             "edit_increment" => 0,// Must become dynamic
@@ -60,13 +61,13 @@ class GuestOrderManager extends AbstractOrderManager
             "base_adjustment_negative" => "0.0000", // ToDo Calculate Prices
             "base_adjustment_positive" => "0.0000", // ToDo Calculate Prices
             "base_shipping_discount_amount" => "0.0000", // ToDo Calculate Prices
-            "base_subtotal_incl_tax" => $quote->subtotal, // ToDo Tax Calculation
-            "base_total_due" => "0.0000", // ToDo Calculate Prices
-            "payment_authorization_amount" => "0.0000", // ToDo Calculate Prices
+            "base_subtotal_incl_tax" => $quote->base_subtotal, // ToDo Tax Calculation
+            "base_total_due" => $quote->base_grand_total,
+            "payment_authorization_amount" => null, // ToDo Calculate Prices
             "shipping_discount_amount" => "0.0000", // ToDo Calculate Prices
-            "subtotal_incl_tax" => $quote->subtotal, // ToDo Tax Calculation
-            "total_due" => "0.0000", // ToDo Calculate Prices
-            "weight" => "0.0000", //ToDo
+            "subtotal_incl_tax" => $quote->base_subtotal, // ToDo Tax Calculation
+            "total_due" => $quote->base_grand_total, // ToDo Calculate Prices
+            "weight" => $this->weight($quote),
             "customer_dob" => null, //ToDo get DateTime if needed
             "increment_id" => $this->incrementId(),
             "applied_rule_ids" => null, //ToDo Must become dynamic
@@ -85,17 +86,17 @@ class GuestOrderManager extends AbstractOrderManager
             "hold_before_state" => null, // ToDo Must become dynamic,
             "hold_before_status" => null, // ToDo Must become dynamic,
             "order_currency_code" => $quote->quote_currency_code,
-            "original_increment_id" => "", //ToDo must become dynamic
-            "relation_child_id" => "", //ToDo must become dynamic
-            "relation_child_real_id" => "", //ToDo must become dynamic
-            "relation_parent_id" => "", //ToDo must become dynamic
-            "relation_parent_real_id" => "", //ToDo must become dynamic
-            "remote_ip" => "", //ToDo must become dynamic
+            "original_increment_id" => null, //ToDo must become dynamic
+            "relation_child_id" => null, //ToDo must become dynamic
+            "relation_child_real_id" => null, //ToDo must become dynamic
+            "relation_parent_id" => null, //ToDo must become dynamic
+            "relation_parent_real_id" => null, //ToDo must become dynamic
+            "remote_ip" => request()->ip(),
             "shipping_method" => $quote->shipping->method,
             "store_currency_code" => $quote->store_currency_code,
-            "store_name" => $store->name,
-            "x_forwarded_for" => "", //ToDo must become dynamic
-            "customer_note" => "", //ToDo must become dynamic
+            "store_name" => $fullStoreName,
+            "x_forwarded_for" => null, //ToDo must become dynamic
+            "customer_note" => null, //ToDo must become dynamic
             "total_item_count" => $quote->getItemsCount(),
             "customer_gender" => null, //ToDo Need to get gender if not guest
             "discount_tax_compensation_amount" => "0.0000", //ToDo ToDo Calculate Prices
@@ -106,11 +107,11 @@ class GuestOrderManager extends AbstractOrderManager
             "base_shipping_incl_tax" => "0.0000", //ToDo ToDo Calculate Prices
             "coupon_rule_name" => null, //ToDo must become dynamic
             "gift_message_id" => null, //ToDo must become dynamic
-            "paypal_ipn_customer_notified" => null, //ToDo must become dynamic
+            "paypal_ipn_customer_notified" => 0, //ToDo must become dynamic
         ];
     }
 
-    protected function getBillingEmail($quote)
+    protected function getBillingEmail(QuoteSessionObject $quote)
     {
         $addresses = $quote->getAddresses();
         foreach ($addresses as $address) {
@@ -120,4 +121,10 @@ class GuestOrderManager extends AbstractOrderManager
         }
         return null;
     }
+
+    protected function weight($quote)
+    {
+        return $quote->weight ? $quote->weight : "0.0000";
+    }
+
 }
