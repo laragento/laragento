12d1d71bce3a306febb616cd2af8a70bf95bc33d
Your Name <you@example.com>
2018-12-18 13:59:06 +0000

    add category images to image helper

--- Catalog/Helpers/ImageHelper.php
+++ Catalog/Helpers/ImageHelper.php
@@ -100,4 +100,68 @@ class ImageHelper
 
         return $result;
     }
+
+
+    public static function runCategoryFileGetContent($execProductIds, $width, $height, $result)
+    {
+        $productIdsParams = implode("=",$execProductIds);
+        $imagesJson = file_get_contents(config('catalog.magento_images_complete_url').'category_image.php?ids='.$productIdsParams.'&width='.$width.'&height='.$height);
+        $images = json_decode($imagesJson, true);
+
+        foreach ($images as $key =>$image){
+            $result[$image['category_id']] = $image['images'][0]['image_url'];
+            Cache::put('category-image-' . $image['category_id'] . '-' . $width . '-' . $height, $image['images'][0]['image_url'], 60); //cache image one day
+        }
+
+        return $result;
+    }
+
+
+    public static function getCategoryImageURL($categoryId, $width, $height) {
+        $images = self::getCategoryImageURLs([$categoryId], $width, $height);
+        return $images[$categoryId];
+    }
+
+    /**
+     * Get images for category image array
+     *
+     * @param $categoryIds
+     * @param $width
+     * @param $height
+     * @return array
+     */
+    public static function getCategoryImageURLs($categoryIds, $width, $height) {
+
+        $result = [];
+        $execCategoryIds = [];
+        //only run exec in production mode
+//        if(App::environment() != 'production') {
+//            foreach($categoryIds as $categoryId) {
+//                $result[$categoryId] = url(config('catalog.placeholder_image_path'));
+//            }
+//
+//            return $result;
+//        }
+
+
+        //get products in cache and set productIds who should be fetched from Magento System
+        $execProductIds = [];
+        foreach($categoryIds as $categoryId) {
+            $cache = Cache::get('category-image-' . $categoryId . '-' . $width . '-' . $height);
+            if($cache != null) {
+                $result[$categoryId] = $cache;
+                continue;
+            }
+            $execCategoryIds[] = $categoryId;
+        }
+        if(count($execCategoryIds) == 0) {
+            return $result;
+        }
+
+        if(config('catalog.magento_access_images_method') == 'file_get_content'){
+            return self::runCategoryFileGetContent($execCategoryIds, $width, $height, $result);
+        }else{
+            return self::runExecute($categoryIds, $width, $height, $result);
+        }
+    }
 }
